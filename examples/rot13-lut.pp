122eea986319cc34d8a3fb60cc044eb7266b56a2
Bertram Felgenhauer <int-e@gmx.de>
2019-05-27 19:58:29 +0200

    another rot13 program

---
+++ examples/rot13-lut.pp
@@ -0,0 +1,15 @@
+,
+>
+  \
+  ~:.~
+  ]8|[11|{8*32}
+  ?
+  | lookup table computed by the Haskell expression (with Data.Bits)
+  | 8*96 .|. sum [(x `xor` y) `shiftL` (8*fromIntegral x)
+  |              | (x, y) <- zip [1..13] [14..] ++ zip [14..] [1..13]]
+  ?
+  ^9495516882517365034402269903000168120394070347137372457794342656
+  ^^]&{8*255}&31[8^]8&255
+  /
+<
+,
